67a1cb6733e883d1bc9278d8c0028f538f448045
Rutu <rutubhimani124@gmail.com>
2024-08-01 21:39:06 +0530

    fix:show post API

--- Backend/src/controller/post.controller.js
+++ Backend/src/controller/post.controller.js
@@ -13,272 +13,299 @@ import mongoose from "mongoose";
 //Upload Post
 
 const uploadPost = asyncHandler(async (req, res) => {
-  const { content, description } = req.body;
-  const user_id = req.user._id;
-  if (!content || !description) {
-    throw new ApiError(400, "Please provide all the required fields");
-  }
-  // console.log(user_id);
-  const post = await Post.create({
-    userId: user_id,
-    content,
-    description,
-  });
-  if (!post) {
-    throw new ApiError(300, "Post not created");
-  }
-  return res
-    .status(200)
-    .json(new ApiResponse(200, post, "Post created successfully"));
+    const { content, description } = req.body;
+    const user_id = req.user._id;
+    if (!content || !description) {
+        throw new ApiError(400, "Please provide all the required fields");
+    }
+    // console.log(user_id);
+    const post = await Post.create({
+        userId: user_id,
+        content,
+        description,
+    });
+    if (!post) {
+        throw new ApiError(300, "Post not created");
+    }
+    return res
+        .status(200)
+        .json(new ApiResponse(200, post, "Post created successfully"));
 });
 
 const likePost = asyncHandler(async (req, res) => {
-  const post_id = req.params.id;
-  // console.log(post_id);
-  const user_id = req.user._id;
-  const post = await Post.findById({ _id: post_id });
-  if (!post) {
-    throw new ApiError(400, "Invalid post id");
-  }
-  if (!user_id) {
-    throw new ApiError(400, "Invalid user id");
-  }
-  // const post = await Post.findById(post_id);
-  const isLiked = await PostLike.findOne({ postId: post_id });
-  if (isLiked) {
-    const postLike = await PostLike.findOneAndDelete({
-      postId: post_id,
-      userId: user_id,
+    const post_id = req.params.id;
+    // console.log(post_id);
+    const user_id = req.user._id;
+    const post = await Post.findById({ _id: post_id });
+    if (!post) {
+        throw new ApiError(400, "Invalid post id");
+    }
+    if (!user_id) {
+        throw new ApiError(400, "Invalid user id");
+    }
+    // const post = await Post.findById(post_id);
+    const isLiked = await PostLike.findOne({ postId: post_id });
+    if (isLiked) {
+        const postLike = await PostLike.findOneAndDelete({
+            postId: post_id,
+            userId: user_id,
+        });
+        return res
+            .status(200)
+            .json(new ApiResponse(200, postLike, "Post disliked successfully"));
+    }
+    const postLike = await PostLike.create({
+        postId: post_id,
+        userId: user_id,
     });
     return res
-      .status(200)
-      .json(new ApiResponse(200, postLike, "Post disliked successfully"));
-  }
-  const postLike = await PostLike.create({
-    postId: post_id,
-    userId: user_id,
-  });
-  return res
-    .status(200)
-    .json(new ApiResponse(200, postLike, "Post liked successfully"));
+        .status(200)
+        .json(new ApiResponse(200, postLike, "Post liked successfully"));
 });
 
 const deletePost = asyncHandler(async (req, res) => {
-  const post_id = req.params.id;
-  const user_id = req.user._id;
-  if (!post_id) {
-    throw new ApiError(400, "Post is not fetched");
-  }
+    const post_id = req.params.id;
+    const user_id = req.user._id;
+    if (!post_id) {
+        throw new ApiError(400, "Post is not fetched");
+    }
 
-  const deletePost = await Post.findOneAndDelete({
-    _id: post_id,
-    userId: user_id,
-  });
-  if (!deletePost) {
-    throw new ApiError(402, "Some happend while deleting post");
-  }
-  const deleteLikes = await PostLike.findOneAndDelete({
-    postId: post_id,
-    userId: user_id,
-  });
-  if (!deleteLikes) {
-    throw new ApiError(402, "Some happend while deleting likes");
-  }
-  return res
-    .status(200)
-    .json(new ApiResponse(200, {}, "Delete post successfully"));
+    const deletePost = await Post.findOneAndDelete({
+        _id: post_id,
+        userId: user_id,
+    });
+    if (!deletePost) {
+        throw new ApiError(402, "Some happend while deleting post");
+    }
+    const deleteLikes = await PostLike.findOneAndDelete({
+        postId: post_id,
+        userId: user_id,
+    });
+    if (!deleteLikes) {
+        throw new ApiError(402, "Some happend while deleting likes");
+    }
+    return res
+        .status(200)
+        .json(new ApiResponse(200, {}, "Delete post successfully"));
 });
 
 const showPosts = asyncHandler(async (req, res) => {
-  try {
-    var page = Number(req.params.page);
-    var post_data;
-    post_data = await Post.aggregate([
-      {
-        $lookup: {
-          from: "postlikes",
-          localField: "_id",
-          foreignField: "postId",
-          as: "likes",
-        },
-      },
-      {
-        $lookup: {
-          from: "users",
-          localField: "userId",
-          foreignField: "_id",
-          as: "user",
-        },
-      },
-      {
-        $addFields: {
-          user: {
-            $arrayElemAt: ["$user", 0],
-          },
-        },
-      },
-      {
-        $project: {
-          likes: {
-            $size: "$likes",
-          },
-          content: 1,
-          description: 1,
-          createdAt: 1,
-          "user.firstName": 1,
-          "user.lastName": 1,
-          "user.initials": 1,
-          "user.image": 1,
-        },
-      },
-      {
-        $sort: {
-          createdAt: -1,
-        },
-      },
-      {
-        $skip: 10 * (page - 1),
-      },
-      {
-        $limit: 10,
-      },
-    ]);
-    return res
-      .status(200)
-      .json(new ApiResponse(200, post_data, "Post data fetched successfully"));
-  } catch (error) {
-    throw new ApiError(400, error);
-  }
+    try {
+        const user_id = req.user._id;
+        var page = Number(req.params.page);
+        var post_data;
+        const id = new mongoose.Types.ObjectId(user_id);
+        post_data = await Post.aggregate([
+            {
+                $lookup: {
+                    from: "postlikes",
+                    localField: "_id",
+                    foreignField: "postId",
+                    as: "likes",
+                },
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "userId",
+                    foreignField: "_id",
+                    as: "user",
+                },
+            },
+            {
+                $addFields: {
+                    user: {
+                        $arrayElemAt: ["$user", 0],
+                    },
+                    isLiked: {
+                        $cond: {
+                            if: {
+                                $in: [id, "$likes.userId"], // Check if loggedInUserId exists in likes.userId array
+                            },
+                            then: true,
+                            else: false,
+                        },
+                    },
+                },
+            },
+            {
+                $project: {
+                    likes: {
+                        $size: "$likes",
+                    },
+                    content: 1,
+                    description: 1,
+                    createdAt: 1,
+                    "user.firstName": 1,
+                    "user.lastName": 1,
+                    "user.initials": 1,
+                    "user.image": 1,
+                    isLiked: 1,
+                },
+            },
+
+            {
+                $sort: {
+                    createdAt: -1,
+                },
+            },
+            {
+                $skip: 10 * (page - 1),
+            },
+            {
+                $limit: 10,
+            },
+        ]);
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    post_data,
+                    "Post data fetched successfully"
+                )
+            );
+    } catch (error) {
+        throw new ApiError(400, error);
+    }
 });
 
 const addComment = asyncHandler(async (req, res) => {
-  const post_id = req.params.id;
-  const user_id = req.user._id;
-  const { comment } = req.body;
-  if (!post_id || !user_id || !comment) {
-    throw new ApiError(400, "Please provide all the required fields");
-  }
-  const newComment = await Comment.create({
-    userId: user_id,
-    postId: post_id,
-    comment,
-  });
-  if (!newComment) {
-    throw new ApiError(300, "Comment not created");
-  }
-  return res
-    .status(200)
-    .json(new ApiResponse(200, newComment, "Comment Added Succesfully"));
+    const post_id = req.params.id;
+    const user_id = req.user._id;
+    const { comment } = req.body;
+    if (!post_id || !user_id || !comment) {
+        throw new ApiError(400, "Please provide all the required fields");
+    }
+    const newComment = await Comment.create({
+        userId: user_id,
+        postId: post_id,
+        comment,
+    });
+    if (!newComment) {
+        throw new ApiError(300, "Comment not created");
+    }
+    return res
+        .status(200)
+        .json(new ApiResponse(200, newComment, "Comment Added Succesfully"));
 });
 
 const likeComment = asyncHandler(async (req, res) => {
-  const comment_id = req.params.id;
-  const user_id = req.user._id;
-  const comment = await Comment.findById({ _id: comment_id });
-  if (!comment) {
-    throw new ApiError(400, "Invalid comment id");
-  }
-  if (!user_id) {
-    throw new ApiError(400, "Invalid user id");
-  }
-  const isLiked = await CommentLike.findOne({ commentId: comment_id });
-  if (isLiked) {
-    const commentLike = await CommentLike.findOneAndDelete({
-      commentId: comment_id,
-      userId: user_id,
+    const comment_id = req.params.id;
+    const user_id = req.user._id;
+    const comment = await Comment.findById({ _id: comment_id });
+    if (!comment) {
+        throw new ApiError(400, "Invalid comment id");
+    }
+    if (!user_id) {
+        throw new ApiError(400, "Invalid user id");
+    }
+    const isLiked = await CommentLike.findOne({ commentId: comment_id });
+    if (isLiked) {
+        const commentLike = await CommentLike.findOneAndDelete({
+            commentId: comment_id,
+            userId: user_id,
+        });
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    commentLike,
+                    "Comment disliked successfully"
+                )
+            );
+    }
+    const commentLike = await CommentLike.create({
+        commentId: comment_id,
+        userId: user_id,
     });
     return res
-      .status(200)
-      .json(new ApiResponse(200, commentLike, "Comment disliked successfully"));
-  }
-  const commentLike = await CommentLike.create({
-    commentId: comment_id,
-    userId: user_id,
-  });
-  return res
-    .status(200)
-    .json(new ApiResponse(200, commentLike, "Comment liked successfully"));
+        .status(200)
+        .json(new ApiResponse(200, commentLike, "Comment liked successfully"));
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
-  const comment_id = req.params.id;
-  const user_id = req.user._id;
-  if (!comment_id || !user_id) {
-    throw new ApiError(400, "Please provide id");
-  }
-  const deleteComment = await Comment.findOneAndDelete({
-    _id: comment_id,
-    userId: user_id,
-  });
-  if (!deleteComment) {
-    throw new ApiError(402, "Some happend while deleting comment");
-  }
-  const deleteLikes = await CommentLike.findOneAndDelete({
-    commentId: comment_id,
-    userId: user_id,
-  });
-  if (!deleteLikes) {
-    throw new ApiError(402, "Some happend while deleting likes");
-  }
-  return res
-    .status(200)
-    .json(new ApiResponse(200, {}, "Delete comment successfully"));
+    const comment_id = req.params.id;
+    const user_id = req.user._id;
+    if (!comment_id || !user_id) {
+        throw new ApiError(400, "Please provide id");
+    }
+    const deleteComment = await Comment.findOneAndDelete({
+        _id: comment_id,
+        userId: user_id,
+    });
+    if (!deleteComment) {
+        throw new ApiError(402, "Some happend while deleting comment");
+    }
+    const deleteLikes = await CommentLike.findOneAndDelete({
+        commentId: comment_id,
+        userId: user_id,
+    });
+    if (!deleteLikes) {
+        throw new ApiError(402, "Some happend while deleting likes");
+    }
+    return res
+        .status(200)
+        .json(new ApiResponse(200, {}, "Delete comment successfully"));
 });
 
 const getComments = asyncHandler(async (req, res) => {
-  const post_id = req.params.id;
-  const post = await Post.findById({ _id: post_id });
-  if (!post) {
-    throw new ApiError(404, "No Post available");
-  }
-  const id = new mongoose.Types.ObjectId(post_id);
-  const comments = await Comment.aggregate([
-    {
-      $match: {
-        postId: id,
-      },
-    },
-    {
-      $lookup: {
-        from: "commentlikes",
-        localField: "_id",
-        foreignField: "commentId",
-        as: "likes",
-      },
-    },
-    {
-      $addFields: {
-        like: {
-          $size: "$likes",
+    const post_id = req.params.id;
+    const post = await Post.findById({ _id: post_id });
+    if (!post) {
+        throw new ApiError(404, "No Post available");
+    }
+    const id = new mongoose.Types.ObjectId(post_id);
+    const comments = await Comment.aggregate([
+        {
+            $match: {
+                postId: id,
+            },
         },
-      },
-    },
-    {
-      $project: {
-        comment: 1,
-        userId: 1,
-        postId: 1,
-        like: 1,
-        createdAt: 1,
-      },
-    },
-  ]);
-  if (!comments || comments.length === 0) {
-    return res.status(202).json(new ApiResponse(202, {}, "Comment not found"));
-  }
+        {
+            $lookup: {
+                from: "commentlikes",
+                localField: "_id",
+                foreignField: "commentId",
+                as: "likes",
+            },
+        },
+        {
+            $addFields: {
+                like: {
+                    $size: "$likes",
+                },
+            },
+        },
+        {
+            $project: {
+                comment: 1,
+                userId: 1,
+                postId: 1,
+                like: 1,
+                createdAt: 1,
+            },
+        },
+    ]);
+    if (!comments || comments.length === 0) {
+        return res
+            .status(202)
+            .json(new ApiResponse(202, {}, "Comment not found"));
+    }
 
-  return res
-    .status(200)
-    .json(new ApiResponse(200, comments, "Comment fetched successfully"));
+    return res
+        .status(200)
+        .json(new ApiResponse(200, comments, "Comment fetched successfully"));
 });
 
 export {
-  uploadPost,
-  likePost,
-  deletePost,
-  showPosts,
-  addComment,
-  likeComment,
-  deleteComment,
-  getComments,
+    uploadPost,
+    likePost,
+    deletePost,
+    showPosts,
+    addComment,
+    likeComment,
+    deleteComment,
+    getComments,
 };
