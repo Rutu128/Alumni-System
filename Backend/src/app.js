575f3cb67b7a224d45aa00565f36eae3c7dc4202
Rutu <rutubhimani124@gmail.com>
2024-09-20 23:37:03 +0530

    fix:deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: [process.env.CLIENT_URL],
+        origin: ["http://37.27.81.8:9001"],
         credentials: true,
         methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS", "UPDATE", "PUT"],
     })
