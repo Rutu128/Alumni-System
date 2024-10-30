3fabb1e0d685e008c96c095a2f49808638f6a771
DevBhimani011 <devbhimani1111@gmail.com>
2024-10-30 09:37:56 +0530

    added job-post controller

--- Backend/src/routes/jobPost.routes.js
+++ Backend/src/routes/jobPost.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
-import { uploadJobPost } from "../controller/jobPost.controller.js";
+import { deleteJobPost, getJob, showJobs, updateJob, uploadJobPost } from "../controller/jobPost.controller.js";
 
 const router = Router();
 
 router.route("/upload-job-post").post(verifyJWT, uploadJobPost);
+router.route("/show-jobs").get(verifyJWT, showJobs);
+router.route("/delete-job-post/:id").delete(verifyJWT, deleteJobPost);
+router.route("/find-job-post/:id").get(verifyJWT, getJob);
+router.route("/update-job-post/:id").put(verifyJWT, updateJob);
 
 export default router
