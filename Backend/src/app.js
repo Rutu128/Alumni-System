fa43cbedd28735d7c9b33a8eeffb3f8a7b2b1969
Rutu <rutubhimani124@gmail.com>
2024-11-16 12:02:09 +0530

    fix: update CORS configuration to allow additional origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: ["http://37.27.81.8:9001", "http://localhost:5174", "http://localhost:5173"],
+        origin: ["http://37.27.81.8:9001", "http://localhost:5174", "http://localhost:5173","http://185.7.81.222:8001"],
         credentials: true,
         methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS", "UPDATE", "PUT"],
     })
