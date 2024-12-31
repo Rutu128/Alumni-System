3303460ca778833a6059440823bf9d1769572fd7
Rutu <rutubhimani124@gmail.com>
2024-12-31 15:15:29 +0530

    fix: update CORS origin to support deployment on alumni-system.pages.dev

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: process.env.CLIENT_URL,
+        origin:"https://alumni-system.pages.dev",
         credentials: true,
         methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS", "UPDATE", "PUT"],
     })
